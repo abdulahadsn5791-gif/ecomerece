5a819506c191863d9a1217851d4bad4b04c6fbf8
abdulahadsn5791-gif <abdulahadsn5791@gmail.com>
2026-09-13 12:42:38 +0500

    Added origin

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.100.110'],
+  allowedDevOrigins: ['192.168.100.110', '108.165.233.182'],
   transpilePackages: ['@ecomerece/domain', '@ecomerece/shared', '@ecomerece/frontend'],
   experimental: {
     optimizePackageImports: ['lucide-react', 'framer-motion', '@ecomerece/frontend'],
